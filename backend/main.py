067ce632828b26e722a9da83bef54ed1513699a6
Nina A <55521586+nina-adhikari@users.noreply.github.com>
2024-06-13 04:48:49 +0000

    querying

--- backend/main.py
+++ backend/main.py
@@ -11,10 +11,10 @@ async def root():
     return query.connect("What is known about Euler characteristics of Hilbert schemes?")
 
 
-# @app.get("/query/{item_id}")
-# def read_item(item_id: int, q: Union[str, None] = None):
-#     return "Connected"
-#     #return query.connect(q)
+@app.get("/query/{q}")
+async def read_item(q: str = None):
+    #return "Connected"
+    return query.connect(q)
 
 
 # @app.put("/items/{item_id}")
