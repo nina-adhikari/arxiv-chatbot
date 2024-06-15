1544957595cbb8bc818e008b51e749f7626ae81b
Nina A <ninaadhikari@proton.me>
2024-06-15 18:40:20 +0000

    updated frontend to add memory and agent

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,34 +27,62 @@ st.markdown(
 
 
 
-WELCOME = "Hello. What would you like to know?"
+WELCOME = "Hello! How can I assist you today?"
 
 ARXIV_URL = "https://arxiv.org/abs/"
 
 st.title("ArXiv Chatbot")
 
+def display(stream, wait):
+    for chunk in stream.json():
+        if chunk == "output":
+            for text in list(stream.json()[chunk]):
+                yield(text)
+                time.sleep(wait)
+        if chunk == "intermediate_steps":
+            steps = stream.json()[chunk]
+            if len(steps) > 0:
+                sources = steps[0][1]['context'] or None
+                yield " \n  "
+                yield " \n  "
+                yield "**Sources**: \n "
+                for i in range(len(sources)):
+                    metadata = sources[i]['metadata'] or None
+                    try:
+                        yield f"{i+1}. [{metadata['title']}]({ARXIV_URL}{metadata['source']}) \n "
+                    except:
+                        pass
+                    time.sleep(wait)
+    return stream
+
 def process_query(prompt):
     query = quote(prompt, safe='/:')
     with st.spinner(""):
-        stream = requests.get(URL+query,
-                            params={'key':'value'}
-                            )
-    counter = 0
-    for chunk in stream.json():
-        if type(chunk) == str:
-            for text in list(chunk):
-                yield text
-                time.sleep(0.01)
-            yield " \n  "
-            yield " \n  "
-            yield "**Sources**: \n "
-        if type(chunk) == dict:
-            try:
-                yield f"{counter}. [{chunk['title']}]({ARXIV_URL}{chunk['source']}) \n "
-            except:
-                pass
-        time.sleep(0.1)
-        counter += 1
+        stream = requests.get(
+            URL+query,
+            params={'key':'value'}
+        )
+    stream = display(stream, 0.01)
+        
+        # region old content
+
+        # counter = 0
+        # if type(chunk) == str:
+        #     for text in list(chunk):
+        #         yield text
+        #         time.sleep(0.01)
+        #     yield " \n  "
+        #     yield " \n  "
+        #     yield "**Sources**: \n "
+        # if type(chunk) == dict:
+        #     try:
+        #         yield f"{counter}. [{chunk['title']}]({ARXIV_URL}{chunk['source']}) \n "
+        #     except:
+        #         pass
+        # time.sleep(0.1)
+        # counter += 1
+
+        # endregion
     return stream
 
 if "messages" not in st.session_state:
